957187f4478664f8155b9e73979d31233fdd84ea
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-17 15:22:50 +0530

    Array Traversal

--- 00_Array/00_Traversal.cpp
+++ 00_Array/00_Traversal.cpp
@@ -3,25 +3,29 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+// Function to create and set the array 
 int setArr (int *Arr, int &n) {
     int x; 
     cout << "Enter the number of elements: ";
     cin >> x ;
 
+    // Cannot enter more elements than available size
     if (x >= n) {
         cout << "Not enough space" << endl;
         return -1;
     }
+    // Take user input 
     for (int i = 0; i < x; ++i)
     {
         cout << "Enter Element " << i + 1 << " : ";
         cin >> Arr[i] ;
     }
+    // Return number of elements in array.
     return x ;
 }
 
-
-void traverseArray(int *A, int &x) {
+// Traversal Function : Traverse and print 
+void traverseArray(int *A, int &x, int &n) {
     for (int i = 0; i < x; ++i)
     {
         cout << A[i] << " " ;
@@ -37,7 +41,7 @@ int main () {
     int *Arr = new int[n];
 
     int x = setArr(Arr, n);
-    traverseArray(Arr, x);
+    traverseArray(Arr, x, n);
 
     return 0;
 }
