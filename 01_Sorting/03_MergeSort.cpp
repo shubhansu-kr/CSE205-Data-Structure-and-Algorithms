f33ce5865cb389bacf7b663c41b0ed3bd26d63d0
Shubhansu kumar <shubhansu2021@gmail.com>
2022-09-29 10:43:52 +0530

    Added Attachment
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_Sorting/03_MergeSort.cpp
+++ 01_Sorting/03_MergeSort.cpp
@@ -4,23 +4,29 @@
 using namespace std ;
 
 void merge(int *Arr, int p, int q, int r) {
-    int n1 = q - p + 1, n2 = r - q;
+    int n1 = q - p + 1, n2 = r - q, n = r - p + 1;
     int L1[n1+1], L2[n2+1];
     
     L1[n1] = INT_MAX;
     L2[n2] = INT_MAX;
 
+    cout << "L1: ";
     for (int i = 0; i < n1; ++i)
     {
         L1[i] = Arr[p+i];
+        cout << L1[i] << " ";
     }
+    cout << "\n";
+    cout << "L2: ";
     for (int j = 0; j < n2; ++j)
     {
         L2[j] = Arr[q+j+1];
+        cout << L2[j] << " ";
     }
-    
+    cout << "\n";
+
     int i =0, j = 0, k = 0;
-    for (int k = 0; k <= r; ++k)
+    for (int k = 0; k < n; ++k)
     {
         if (L1[i] < L2[j]) {
             Arr[k] = L1[i++];
@@ -40,7 +46,7 @@ void mergeSort(int *Arr, int p, int r) {
 }
 
 int main () {
-    int Arr[] = {4, 2, 5, 7, 1, 9};
+    int Arr[] = {0, 1, 2, 3, 4, 5};
     mergeSort(Arr, 0, 5);
     for(auto it: Arr){
         cout << it << " ";
