afab6925c45bc0bdb70d0d1225283f48762a2d7a
Shubhansu kumar <shubhansu2021@gmail.com>
2022-10-04 20:19:59 +0530

    QuickSOrt
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 01_Sorting/04_QuickSort.cpp
@@ -0,0 +1,29 @@
+// QuickSort
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+int partition(int *Arr, int p, int r) {
+    int x = Arr[r], i = p-1;
+    for (int j = p; j < r; ++j)
+    {
+        if (Arr[j] <= x) {
+            swap(Arr[++i], Arr[j]);
+        }
+    }
+    swap(Arr[i++], Arr[r]);
+    return i;
+}
+
+void quickSort(int *Arr, int p, int r){
+    if (p > r) return;
+    int q = partition(Arr, p, r);
+    quickSort(Arr, p, q-1);
+    quickSort(Arr, q+1, r);
+}
+
+int main () {
+    int Arr[] = {6, 4, 2, 8, 5, 5};
+
+    return 0;
+}
